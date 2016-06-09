b841d35bbb38505a004c19bcfc406a47d6657a11
Dave Gould <dgould@modusagency.com>
2016-06-09 10:33:14 -0400

    organizational changes

---
+++ scripts/script.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', function () {
+  if (document.querySelectorAll('#map').length > 0)
+  {
+    var js_file = document.createElement('script');
+    js_file.type = 'text/javascript';
+    js_file.src = 'https://maps.googleapis.com/maps/api/js?callback=initMap&signed_in=true&key=AIzaSyBJyAOB0B2vN58rzCjVAN7VToMlEgdVIW4';
+    document.getElementsByTagName('head')[0].appendChild(js_file);
+  }
+});
+
+var map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 41.811200, lng: -73.262314},
+    zoom:18,
+    mapTypeId:google.maps.MapTypeId.HYBRID
+  });
+
+  fetch('markers.json')
+    .then(function(response){return response.json()})
+    .then(plotMarkers);
+}
+
+var markers;
+var bounds;
+
+function plotMarkers(m) {
+  markers = [];
+  bounds = new google.maps.LatLngBounds();
+
+  m.forEach(function (marker) {
+    var position = new google.maps.LatLng(marker.lat, marker.lng);
+
+    markers.push(
+      new google.maps.Marker({
+        position: position,
+        map: map,
+        animation: google.maps.Animation.DROP
+      })
+    );
+    bounds.extend(position)
+  });
+  map.fitBounds(bounds);
+}
