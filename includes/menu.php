7933d4665e99463b9b66d7987907d8adbbcd7c07
Dave Gould <dgould@modusagency.com>
2016-06-16 11:38:17 -0400

    versioning all files

---
+++ includes/menu.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+  <link href="styles/main.css" rel="stylesheet" type="text/css" />
+</head>
+<ul>
+  <li><a href="index.html">Home</a></li>
+  <li><a href="fishing.html">Fishing</a></li>
+  <li><a href="skiing.html">Skiing</a></li>
+</ul>
+</html>
